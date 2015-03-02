19846a82353650cc1366909a7d7de7508ee9d0b6
olga-jane <olga.jane.vakhromova@gmail.com>
2015-03-03 01:28:00 +0200

    files for some entities

---
+++ sources/HumanResourcesLibrary/DataClasses/ContactWithCandidate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourcesLibrary.DataClasses
+{
+    public enum ContactTypes
+    { 
+        Meet,
+        Call
+    }
+
+    class ContactWithCandidate
+    {
+    }
+}
