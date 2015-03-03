8315231466768c106979724831d34849e8ded16b
Gorban Nastya <Urianna.Rel@gmail.com>
2015-03-03 15:44:55 +0200

    changes in SocialNetwork, add enum SocialNetworkType

---
+++ sources/HumanResourcesLibrary/DataClasses/SocialNetworkType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourcesLibrary.DataClasses
+{
+    public enum SocialNetworkType
+    {
+        LinkedIn,
+        Facebook,
+        VKontakte,
+        GooglePlus
+    }
+}
