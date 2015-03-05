435e84dbf01d487081ce6b198ce94d9e74017b06
Paradaimu <arcanumthebest@gmail.com>
2015-03-05 19:35:09 +0200

    Добавил привязку к данным.

--- sources/CandidatesSearcher/Form1.cs
+++ sources/CandidatesSearcher/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HumanResourcesLibrary.DataClasses;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.Xpf.Editors;
+
 
 namespace CandidatesSearcher
 {
@@ -20,7 +24,17 @@ namespace CandidatesSearcher
             InitializeComponent();
             vm = new CandidateSearchVievModel();
             command = new CandidateSearchCommand(vm);
-
+            ComboBoxItemCollection updater = CandidateEnglishLevel.Properties.Items;
+            updater.BeginUpdate();
+            updater.Add(new ComboBoxAdd(EnglishLevel.Beginner, "Beginer"));
+            updater.Add(new ComboBoxAdd(EnglishLevel.Elementary, "Elementary"));
+            updater.Add(new ComboBoxAdd(EnglishLevel.Intermediate, "Intermediate"));
+            updater.Add(new ComboBoxAdd(EnglishLevel.PreIntermediate, "PreIntermediate"));
+            updater.Add(new ComboBoxAdd(EnglishLevel.UpperIntermediate, "UpperIntermediate"));
+            updater.Add(new ComboBoxAdd(EnglishLevel.Proficiency, "Proficiency"));
+            updater.EndUpdate();
+           //updater.SelectedIndex = -1;
+           
         }
 
         private void SearchButton_Click(object sender, EventArgs e)
@@ -31,14 +45,46 @@ namespace CandidatesSearcher
         private void Form1_Load(object sender, EventArgs e)
         {
             bindingSource.DataSource = vm;
-            CandidateName.DataBindings.Add("CandidateName", bindingSource, "CandidateName");
-            CandidateSoname.DataBindings.Add("CandidateSoname", bindingSource, "CandidateSoname");
-            CandidatePhone.DataBindings.Add("CandidatePhone", bindingSource, "CandidatePhone");
-            CandidateCity.DataBindings.Add("CandidateCity", bindingSource, "CandidateCity");
-            CandidateEnglishLevel.DataBindings.Add("CandidateEnglishLevel", bindingSource, "CandidateEnglishLevel");
-            
-           // CandidateName.DataBindings.Add("",);
+            CandidateName.DataBindings.Add("EditValue", bindingSource, "CandidateName");
+            CandidateSoname.DataBindings.Add("EditValue", bindingSource, "CandidateSoname");
+            CandidatePhone.DataBindings.Add("EditValue", bindingSource, "CandidatePhone");
+            CandidateCity.DataBindings.Add("EditValue", bindingSource, "CandidateCity");
+            //CandidateEnglishLevel.DataBindings.Add("EditValue", bindingSource, "CandidateEnglishLevel");    
+            CandidateFromDate.DataBindings.Add("EditValue", bindingSource, "CandidateFromDate");
+            CandidateToDate.DataBindings.Add("EditValue", bindingSource, "CandidateToDate");
+            AgreeToRelocate.DataBindings.Add("EditValue", bindingSource, "AgreeToRelocate");
         }
+        
+            class ComboBoxAdd
+            {
+                EnglishLevel level;
+                string name;
+                public ComboBoxAdd(EnglishLevel level, string name)
+                {
+                    this.level = level;
+                    this.name = name;
+                }
+
+                public override string ToString()
+                {
+                    return name;
+                }
+
+                public EnglishLevel GetLevel()
+                {
+                    return level;
+                }
+            }
+        
+
+     
+
+      private void CandidateEnglishLevel_EditValueChanged(object sender, EventArgs e)
+      {
+         // ComboBoxAdd p = ((sender as ComboBoxEdit).SelectedItem as ComboBoxAdd);
+      }
+
+
 
 
         
