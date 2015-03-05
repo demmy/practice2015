35936983b4a87a40c9feda1c4f377609871418dd
vlad <v.bekleneshev@gmail.com>
2015-03-05 16:04:31 +0200

    First version of ContactParserLibrary: Added: 1) Folder "ReadWriteLibrary" with .cs files:  - IReader.cs  - Masks.cs  - ReaderTxt.cs  - ReaderXml.cs 2) Folder "ContactsLibrary" with .cs files:  - Contact.cs  - ContactType.cs  - ContactsStorage.cs  - ContactsStorage.cs
    Also added file ViewModel/CandidatesParserViewModel.cs in the project "CandidatesParser"
    
    And was deleted a file HelloGitHub.txt. This file was a such file for training.

---
+++ sources/CandidatesParser/ViewModel/CandidatesParserViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactParserLibrary.ContactsLibrary;
+using ContactParserLibrary.ReadWriteLibrary;
+
+namespace CandidatesParser
+{
+    class CandidatesParserViewModel    // Like Singelton
+    {
+        //////////////////////////////      SINGELTON       //////////////////////////////////////
+        private static CandidatesParserViewModel instance = null;
+        // Конструкторы и оператор присваивания недоступны клиентам
+        private CandidatesParserViewModel() { }
+        
+        public static CandidatesParserViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CandidatesParserViewModel();
+                }
+                return instance;
+            }
+        }
+
+        //this manager is speak with ContactsManager object
+        ContactsManager myContactsManager = new ContactsManager();
+        public List<KeyValuePair<string, bool>> GetSkypes()
+        {
+            return myContactsManager.GetSkypes();
+        }
+        public List<KeyValuePair<string, bool>> GetEmails()
+        {
+            return myContactsManager.GetEmails();
+        }
+        public List<KeyValuePair<string, bool>> GetPhoneNumbers()
+        {
+            return myContactsManager.GetPhoneNumbers();
+        }
+        public List<KeyValuePair<string, bool>> GetWebSites()
+        {
+            return myContactsManager.GetWebSites();
+        }
+        //set true for selected contact
+        public void SelectSkype(int index)
+        {
+            myContactsManager.SelectSkype(index);
+        }
+        public void SelectEmail(int index)
+        {
+            myContactsManager.SelectEmail(index);
+        }
+        public void SelectWebSite(int index)
+        {
+            myContactsManager.SelectWebSite(index);
+        }
+        public void SelectPhoneNumbers(int[] index)
+        {
+            myContactsManager.SelectPhoneNumbers(index);
+        }
+        public void Read(string path)
+        {
+            myContactsManager.Read(path);
+        }
+    }
+}
---
+++ sources/ContactParserLibrary/ReadWriteLibrary/IReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactParserLibrary.ContactsLibrary;
+
+namespace ContactParserLibrary.ReadWriteLibrary
+{
+    public interface IReader
+    {
+        void InitContactsStorage(out ContactsStorage storage);
+    }
+}
