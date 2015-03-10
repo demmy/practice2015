30bca2e040ff3f26065f7690dbb47269153954c0
Paradaimu <arcanumthebest@gmail.com>
2015-03-10 12:16:52 +0200

    Рефакторинг.

--- sources/CandidatesSearcher/CandidateSearchCommand.cs
+++ sources/CandidatesSearcher/CandidateSearchCommand.cs
@@ -36,56 +36,7 @@ namespace CandidatesSearcher
 
             viewModel.SortedCandidate = new ObservableCollection<Candidate>(value);
             viewModel.View.Refresher();
-            
-            /*if (!string.IsNullOrWhiteSpace(viewModel.CandidateName))
-            { 
-                FormInfo.Add("Name", viewModel.CandidateName);
-            }
-
-            if (!string.IsNullOrWhiteSpace(viewModel.CandidateSoname))
-            {
-                FormInfo.Add("Soname", viewModel.CandidateSoname);
-            }
-
-            if (!string.IsNullOrWhiteSpace(viewModel.CandidateCity))
-            {
-                FormInfo.Add("City", viewModel.CandidateCity);
-            }
-
-            if (!string.IsNullOrWhiteSpace(viewModel.CandidatePhone))
-            {
-                FormInfo.Add("Phone", viewModel.CandidatePhone);
-            }
-
-            if (viewModel.EngLevel != null)
-            {
-                FormInfo.Add("EngLevel", viewModel.EngLevel);
-            }
-
-            if (viewModel.AgreeToRelocate!= null)
-            {
-                FormInfo.Add("AgreeToRelocate", viewModel.AgreeToRelocate);
-            }
-
-            if (viewModel.FromDate != default (DateTime))
-            {
-                FormInfo.Add("FromDate", viewModel.FromDate);
-            }
-
-            if (viewModel.ToDate != default(DateTime))
-            {
-                FormInfo.Add("ToDate", viewModel.ToDate);
-            }*/
         }
-
-
-        /*public Dictionary<string, object> GetFormInformation
-        {
-            get 
-            {
-                return FormInfo;
-            }
-        }*/
 
         public bool CanExecute(object o)
         {
